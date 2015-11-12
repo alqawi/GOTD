5e2146b6667b8ba1099e7035939fa5084926608b
Abdullah Alqawi <abdullahalqawi@Abdullahs-MacBook-Pro.local>
2015-11-11 22:48:07 -0400

    GOTD-mainpage

---
+++ GOTD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GOTD
+//
+//  Created by Abdullah Alqawi on 2015-11-11.
+//  Copyright © 2015 Abdullah Alqawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
